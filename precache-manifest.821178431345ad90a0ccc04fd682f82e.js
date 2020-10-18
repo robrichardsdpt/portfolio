a5a89dba4fa98d6dba8dee473419311f4e86a7f8
robrichardsdpt <robrichardsdpt@gmail.com>
2020-10-17 20:59:36 -0400

    Updates

---
+++ precache-manifest.821178431345ad90a0ccc04fd682f82e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91071de8fe1c88d2fb3d3502f7f91218",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9c6bcf450343f6e6b4f5",
+    "url": "/portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1d64b844ffa34fe53ba2",
+    "url": "/portfolio/static/css/main.1c4c92e7.chunk.css"
+  },
+  {
+    "revision": "9c6bcf450343f6e6b4f5",
+    "url": "/portfolio/static/js/2.9fa0e05d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.9fa0e05d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d64b844ffa34fe53ba2",
+    "url": "/portfolio/static/js/main.67d668ce.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "fa7362e5fd2aaeed1f8adec6c8ab64cc",
+    "url": "/portfolio/static/media/AJAX-Language-01.fa7362e5.png"
+  },
+  {
+    "revision": "dce771fee28899e44125b7ec8f575622",
+    "url": "/portfolio/static/media/Django-01.dce771fe.png"
+  },
+  {
+    "revision": "8d6f57405dfa1b490e556c34790991b8",
+    "url": "/portfolio/static/media/HTML.8d6f5740.png"
+  },
+  {
+    "revision": "bb8e6034b2c49c28e92aee02edba0576",
+    "url": "/portfolio/static/media/JQuery-01.bb8e6034.png"
+  },
+  {
+    "revision": "6bc7d60a8b7c8d5d8f0e0b822331293e",
+    "url": "/portfolio/static/media/Markdown-01.6bc7d60a.png"
+  },
+  {
+    "revision": "0c3452f69be25b8c1926af57d8ae90fd",
+    "url": "/portfolio/static/media/MongoDB-01.0c3452f6.png"
+  },
+  {
+    "revision": "8f343e6a225cac22895e5deb21fd52c9",
+    "url": "/portfolio/static/media/Node-JS-01.8f343e6a.png"
+  },
+  {
+    "revision": "230c80bff87a962a6279e475f6da39cf",
+    "url": "/portfolio/static/media/RRichardsfull.230c80bf.jpg"
+  },
+  {
+    "revision": "54d4c686dbd729b3fab40c4448bcaba9",
+    "url": "/portfolio/static/media/Sass-01.54d4c686.png"
+  },
+  {
+    "revision": "83553b6d96feda7c11a0cf2389d3e64e",
+    "url": "/portfolio/static/media/bondcliff.83553b6d.jpg"
+  },
+  {
+    "revision": "8f047bb893c3c4ee94de0832935d7c89",
+    "url": "/portfolio/static/media/css3-logo.8f047bb8.png"
+  },
+  {
+    "revision": "899e2d992f10b52a44338f4149127b7c",
+    "url": "/portfolio/static/media/email.899e2d99.png"
+  },
+  {
+    "revision": "5575261e512cd863df2cd6596efb2dab",
+    "url": "/portfolio/static/media/hike-log.5575261e.jpg"
+  },
+  {
+    "revision": "50972978c30dab21408784ba57aac04a",
+    "url": "/portfolio/static/media/instasham.50972978.jpg"
+  },
+  {
+    "revision": "50a67f1cbe0f46b4cd999a8d083155bc",
+    "url": "/portfolio/static/media/javaScript-logo.50a67f1c.png"
+  },
+  {
+    "revision": "cb60e778aed36744ff1ccdeb2789d02c",
+    "url": "/portfolio/static/media/mobile-phone-760.cb60e778.png"
+  },
+  {
+    "revision": "3cf229eedc092549277e8859aad2fca5",
+    "url": "/portfolio/static/media/python-logo.3cf229ee.png"
+  },
+  {
+    "revision": "f1ab063665f9c97030d14afeefa7f455",
+    "url": "/portfolio/static/media/tic-tac-toe.f1ab0636.jpg"
+  }
+]);
