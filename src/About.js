9580b8dbab42f0f0e0a5dfae0b3a1f0f2d4224ed
Rob <robrichardsdpt@gmail.com>
2020-11-21 08:25:47 -0500

    added text shadow and spans to improve look of about section

--- src/About.js
+++ src/About.js
@@ -15,7 +15,7 @@ const About = () => (
     </Row>
     <Row>
     <Col s={12} md={6} className="aboutText">
-      I am a full-stack software engineer with a professional background in physical therapy, rehabilitation management, and leadership. I enjoy a challenge and working as a part of a team to create new technology. I am proficient in JavaScript, Python, HTML, CSS and React. I am driven by self and professional development to become a leader in the field. Further, I am motivated to create software and technology to help others accomplish their goals, and to improve the human experience.  I am looking to utilize my technical skills, along with those that I have obtained as a physical therapist to contribute to the world of technology.
+      I am a <span className="highlight">full-stack software engineer</span> with a professional background in <span className="highlight">physical therapy</span>, rehabilitation management, and leadership. I enjoy a challenge and working as a part of a <span className="highlight">team</span> to create new technology. I am proficient in <span className='highlight'>JavaScript</span>, Python, HTML, CSS and React. I am driven by self and professional development to become a <span className="highlight">leader</span> in the field. Further, I am motivated to create software and technology to help others accomplish their goals, and to <span className="highlight">improve the human experience</span>.  I am looking to utilize my technical skills, along with those that I have obtained as a physical therapist to contribute to the world of technology.
     </Col>
       <Col s={12} md={6} className='aboutPic'>
         <Image src={self} alt="About Me" className='self' roundedCircle/>
