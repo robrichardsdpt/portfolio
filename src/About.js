78a90c9be1e0884b50b4bcd4bc72f1e1ac7c78f1
Rob <robrichardsdpt@gmail.com>
2020-12-10 14:54:12 -0500

    updated formatting of site to look better on phones

--- src/About.js
+++ src/About.js
@@ -7,11 +7,13 @@ import self from './RRichardsfull.jpg'
 const About = () => (
   <div className="aboutContainer" id='about'>
   <div className="about">
+
     <Row>
       <Col className="namecol">
         <h1>Hi, my name is <span className="name">Rob</span>.</h1>
       </Col>
     </Row>
+
     <Row>
     <Col s={12} md={6} className="aboutText">
       <p>I am a <span className="highlight">full-stack software engineer</span> with a professional background in <span className="highlight">physical therapy</span>, rehabilitation management, and leadership.</p><p> I enjoy a challenge and working as a part of a <span className="highlight">team</span> to create new technology.</p><p>I am proficient in <span className='highlight'>JavaScript</span>, Python, HTML, CSS and React.</p><p>I am driven by self and professional development to become a <span className="highlight">leader</span> in the field. </p><p>Further, I am motivated to create software and technology to help others accomplish their goals, and to <span className="highlight">improve the human experience</span>. </p><p> I am looking to utilize my technical skills, along with those that I have obtained as a physical therapist to contribute to the world of <span className="highlight">technology</span>.</p>
@@ -20,6 +22,7 @@ const About = () => (
         <Image src={self} alt="About Me" className='self' roundedCircle/>
       </Col>
     </Row>
+    
   </div>
 </div>
 )
